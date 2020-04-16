868762da30cbbb95e69972088737dbf9da3525e6
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-16 15:21:19 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello mate academy!');
+const thumbs = document.querySelector('#thumbs');
+const img = document.querySelector('#largeImg');
+
+thumbs.onclick = function() {
+  const thumbnail = event.target.closest('.list-item__link');
+
+  if (!thumbnail) {
+    return;
+  }
+  showThumbnail(thumbnail.href, thumbnail.title);
+  event.preventDefault();
+};
+
+function showThumbnail(href, title) {
+  img.src = href;
+  img.alt = title;
+}
